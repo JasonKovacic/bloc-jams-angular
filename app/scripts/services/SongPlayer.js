88714a6c811df9656e215096afa952a938f29202
Jason <kovacic.jason@gmail.com>
2016-10-20 18:32:36 -0700

    Checkpoint 7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,18 +1,57 @@
 (function() {
-     function SongPlayer() {
-          var SongPlayer = {};
-          SongPlayer.play = function(song) {
-              var currentBuzzObject = new buzz.sound(song.audioUrl, {
-                  formats: ['mp3'],
-                  preload: true
-              });
+    function SongPlayer() {
+        //*
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
 
-              currentBuzzObject.play();
-          };
-          return SongPlayer;
-     }
 
-     angular
-         .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
- })();
+        var SongPlayer = {};
+
+        var currentSong = null;
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
+        var currentBuzzObject = null;
+
+        /**
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
+        var setSong = function(song) {
+           if (currentBuzzObject) {
+               currentBuzzObject.stop();
+               currentSong.playing = null;
+           }
+
+           currentBuzzObject = new buzz.sound(song.audioUrl, {
+               formats: ['mp3'],
+               preload: true
+           });
+
+           currentSong = song;
+        };
+
+
+
+        SongPlayer.play = function(song) {
+            if (currentSong !== song) {
+                setSong(song);
+                playSong(song);
+            } else if (currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    currentBuzzObject.play();
+                }
+            }
+        };
+
+        return SongPlayer;
+    }
+
+    angular
+        .module('blocJams')
+        .factory('SongPlayer', SongPlayer);
+})();
